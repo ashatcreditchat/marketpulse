1b69f0edde8c0560b0646a2c1e47d7043045793a
ashatrealmoneyleads <ash@realmoneyleads.com>
2024-02-21 15:28:26 +1000

    Initial commit

---
+++ sentiment_analysis_frontend/src/pages/Index.jsx
@@ -0,0 +1,317 @@
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Flex,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+  VStack,
+  HStack,
+  Stat,
+  StatLabel,
+  StatHelpText,
+  StatArrow,
+  useToast,
+  Icon,
+  Grid,
+  Tooltip,
+  Tag,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+} from "@chakra-ui/react";
+import { FaSearch, FaCircle, FaArrowRight } from "react-icons/fa";
+import SentimentPieChart from "../components/PieChartSentiments";
+import credentials from "../../credentials.json";
+
+const CustomCard = React.forwardRef(({ children, ...rest }, ref) => (
+  <Box p='1'>
+    <Tag ref={ref} {...rest}>
+      {children}
+    </Tag>
+  </Box>
+))
+
+const Index = () => {
+  const [searchKey, setSearchKey] = useState("");
+  const [articleResults, setArticleResults] = useState([]);
+  const [sentimentSumResults, setSentimentSumResults] = useState([]);
+  const [todaysArticles, setTodaysArticles] = useState([]);
+  const [maxArticlesPerSearch, setMaxArticlesPerSearch] = useState(undefined);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  const sentimentSumByDate = () => {
+    const sentimentMap = {
+      'Positive': 1,
+      'Negative': -1,
+      'Neutral': 0,
+    };
+    const groupedByDate = {};
+
+    const today = new Date();
+
+    articleResults.forEach((article) => {
+      const dateString = article.posted.toISOString().split('T')[0];
+      if (!groupedByDate[dateString]) {
+        groupedByDate[dateString] = 0;
+      }
+      groupedByDate[dateString] += sentimentMap[article.sentiment];
+
+      if (article.posted.toISOString().split('T')[0] === today.toISOString().split('T')[0]) {
+        setTodaysArticles((prev) => [...prev, article]);
+      }
+    });
+
+
+    const sentimentSumResult = Object.entries(groupedByDate).map(([date, sentimentSum]) => ({
+      date: new Date(date),
+      sentimentSum: sentimentSum,
+    }));
+    setSentimentSumResults(sentimentSumResult);
+  };
+
+  const handleSearchKeyChange = (event) => {
+    setSearchKey(event.target.value);
+  };
+
+  const handleMaxNumArticleChange = (val) => {
+    if (val > 100) {
+      val = 100;
+    }
+    setMaxArticlesPerSearch(Number(val));
+  }
+
+  const fetchSentiments = async (data) => {
+    if (data.length === 0) {
+      throw new Error("No data was found for the past week")
+    }
+    let body = {
+      inputs: data,
+      options: {
+        wait_for_model: true
+      }
+    }
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/Artanis1551/bert_sentiment_trainer",
+      {
+        headers: { Authorization: `Bearer ${credentials.huggingface}` },
+        method: "POST",
+        body: JSON.stringify(body),
+      }
+    );
+
+    if (response.status === 503) {
+      throw new Error(`The model is currently loading. Please try again in ${result.estimated_time || 20} seconds.`)
+    }
+    else if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    else if (response.status === 200) {
+      const result = await response.json();
+      return result
+    }
+  }
+
+  function getLabelOfMaxScore(jsonList) {
+    let maxScoreJson = jsonList.reduce((max, obj) => (max.score > obj.score ? max : obj), jsonList[0]);
+    return maxScoreJson.label;
+  }
+
+  const analyzeSentiment = async () => {
+    setIsLoading(true);
+    try {
+      const baseUrl = "https://speech-recognition-386209.ew.r.appspot.com"
+      // const baseUrl = "http://localhost:3000"
+
+      const response = await fetch(`${baseUrl}/analyze-sentiment?${new URLSearchParams({
+        searchKey: searchKey,
+        maxArticlesPerSearch: maxArticlesPerSearch
+      })}`, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      let responseJson = await response.json();
+
+      let results = responseJson.result
+      let sentiments = await fetchSentiments(results.map((result) => result.headline))
+
+      results.forEach((result, index) => {
+        result.sentiment = getLabelOfMaxScore(sentiments[index])
+        result.posted = new Date(result.posted)
+      })
+
+      setSentimentSumResults([])
+      setTodaysArticles([])
+      setArticleResults(results);
+
+      toast({
+        title: "Analysis complete.",
+        description: "Sentiment analysis has been successfully completed.",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error) {
+      toast({
+        title: "Analysis failed.",
+        description: `There was a problem with sentiment analysis: ${error.message}`,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (articleResults.length > 0) {
+      sentimentSumByDate();
+    }
+  }, [articleResults])
+
+  return (
+    <Box p={5}>
+      <VStack spacing={4} align="stretch">
+        <Heading as="h1">Sentiment Analysis Dashboard</Heading>
+        <Heading as='h4' size='md'>This algorithm scrapes Yahoo news finance based on your search query. Enter a company name and press the <b>Analyze</b> button. Keep in mind that scraping can take a couple of minutes!</Heading>
+        <FormControl id="searchKey">
+          <HStack>
+            <VStack width="100%">
+              <FormLabel>Search Key</FormLabel>
+              <VStack mb={3} width="30%">
+                <Input type="text" placeholder="Enter a search key e.g. TSLA" value={searchKey} onChange={handleSearchKeyChange} />
+              </VStack>
+              <FormLabel>Enter a maximum number of articles to analyze</FormLabel>
+              <VStack mb={3} width="30%">
+                <NumberInput value={maxArticlesPerSearch} step={5} min={0} max={100} onChange={handleMaxNumArticleChange} width="100%">
+                  <NumberInputField placeholder="Defaults to 50" title="Maximum input is 100" />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
+                <Button isLoading={isLoading} leftIcon={<FaSearch />} colorScheme="teal" onClick={analyzeSentiment} isDisabled={!searchKey || isLoading} >
+                  Analyze
+                </Button>
+              </VStack>
+            </VStack>
+          </HStack>
+        </FormControl>
+
+        {/* Add a pie chart representation */}
+        {
+          todaysArticles && articleResults.length > 0 && (
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4}>
+              <Heading as="h3" size="md" mb={4} textAlign="center">
+                Today's Sentiment Breakdown (%)
+              </Heading>
+              {
+                todaysArticles.length > 0 ? (
+                  <SentimentPieChart data={todaysArticles} />
+                ) :
+                  <Box textAlign="center">
+                    No articles found for today
+                  </Box>
+              }
+            </Box>
+          )
+        }
+
+        {/* Add a summarized bar chart representation */}
+        {
+          sentimentSumResults && sentimentSumResults.length > 0 && (
+            <Box borderWidth="1px" borderRadius="lg" overflow="hidden" p={4}>
+              <HStack spacing={5} alignItems="center">
+                <Heading as="h3" size="md" mb={4} >
+                  Sentiment Score Summary of Found Articles
+                </Heading>
+                <Tooltip label='Here we sum the sentiment scores. A positive score is counted as +1, a negative as -1, and a neutral as 0'>
+                  <Box width="10%" p={2}>
+                    <CustomCard>Info</CustomCard>
+                  </Box>
+                </Tooltip>
+              </HStack>
+
+              <VStack width="full">
+                {sentimentSumResults.map(({ date, sentimentSum }, index) => (
+                  <HStack key={index} width="100%">
+
+                    <Box textAlign="left" width="5em">
+                      {date.toLocaleDateString()}
+                    </Box>
+                    <HStack width="95%">
+                      <Box height="1.5em" width={`${Math.abs(sentimentSum) * 5}%`} bg={
+                        sentimentSum > 0 ? 'green.300'
+                          : sentimentSum < 0 ? 'red.300'
+                            : 'gray.300'
+                      } />
+                      <Box textAlign="center">
+                        {sentimentSum}
+                      </Box>
+                    </HStack>
+                  </HStack>
+                ))}
+              </VStack>
+            </Box>
+          )
+        }
+
+        <Grid templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }} gap={6}>
+          {articleResults && articleResults
+            .sort((a, b) => b.posted - a.posted)
+            .map((result, index) => (
+              <Box key={index} borderWidth="0.5px" borderRadius="lg" p={4}>
+                <Stat>
+                  <StatLabel fontSize="1.2em" marginBottom="0.2em">{result.headline}</StatLabel >
+                  <StatLabel fontSize="1em" fontWeight="0.1em" marginBottom="0.5em">{result.text}</StatLabel >
+                  <StatHelpText >
+                    <HStack>
+                      {
+                        result.sentiment !== "Neutral" ? (
+                          <StatArrow type={result.sentiment === 'Positive' ? 'increase' : 'decrease'} />
+                        ) :
+                          <Icon as={FaCircle} color="#BFB5B5" marginRight="0.4em" />
+                      }
+                      <Box>
+                        {result.sentiment === 'Positive' ? 'Positive' : result.sentiment === 'Negative' ? 'Negative' : 'Neutral'} in sentiment
+                      </Box>
+                      <Box ml="4">
+                        {result.posted.toLocaleDateString()}
+                      </Box>
+                    </HStack>
+                  </StatHelpText>
+                </Stat>
+                <Flex justifyContent="flex-end">
+                  <a href={result.href} target="_blank" rel="noopener noreferrer">
+                    <Button rightIcon={<FaArrowRight />}>
+                      Read
+                    </Button>
+                  </a>
+
+                </Flex>
+              </Box>
+
+            ))}
+        </Grid>
+      </VStack >
+    </Box >
+  );
+};
+
+
+
+export default Index;
---
+++ yahoo_finance_news_scraper.py
@@ -0,0 +1,167 @@
+from transformers import GPT2TokenizerFast, pipeline
+from sklearn.model_selection import train_test_split
+from time import sleep
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime, timedelta
+from tqdm import tqdm
+
+
+def parse_time(posted):
+    # Split the string into value and unit
+    value, unit, _ = posted.split()
+
+    # Convert the value to an integer
+    value = int(value)
+
+    # Calculate the datetime object based on the unit
+    if "minute" in unit:
+        time = datetime.now() - timedelta(minutes=value)
+    elif "hour" in unit:
+        time = datetime.now() - timedelta(hours=value)
+    elif "day" in unit:
+        time = datetime.now() - timedelta(days=value)
+    else:
+        return None
+
+    return time
+
+
+def get_article(card, from_date):
+    """Extract article information from the raw html"""
+    headline = card.find("h4", "s-title").text
+    posted = card.find("span", "s-time").text.replace("·", "").strip()
+    text = card.find("p", "s-desc").text.strip()
+    a_element = card.find("a", "thmb")
+    if a_element:
+        headline = a_element.get("title")
+        href = a_element.get("href")
+    else:
+        return None
+
+    posted = parse_time(posted)
+    if posted < from_date:
+        return None
+
+    article = {
+        "headline": headline,
+        "posted": posted,
+        "text": text.replace("...", ""),
+        "href": href,
+    }
+    return article
+
+
+def get_news_headlines(search_companies, from_date, max_articles_per_search):
+    """
+    Get the news headlines for the companies in the search_companies list
+    from the from_date until today's date. The maximum number of articles
+    returned per company is max_articles_per_search.
+
+    """
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "referer": "https://www.google.com",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44",
+    }
+
+    if not isinstance(search_companies, list):
+        search_companies = [search_companies]
+
+    company_articles = {}
+    for search in tqdm(search_companies):
+        print(f"Collecting articles for {search}")
+        template = "https://news.search.yahoo.com/search?p={}"
+        url = template.format(search)
+        articles = []
+        links = set()
+        counter = 0
+
+        while True:
+            response = requests.get(url, headers=headers)
+            soup = BeautifulSoup(response.text, "html.parser")
+            cards = soup.find_all("div", "NewsArticle")
+
+            # extract articles from page
+            for card in cards:
+                article = get_article(card, from_date)
+                if article:
+                    link = article["href"]
+                    if not link in links and counter < max_articles_per_search:
+                        links.add(link)
+                        del article["href"]
+                        articles.append(article)
+                        counter += 1
+
+            # find the next page
+            try:
+                url = soup.find("a", "next").get("href")
+                sleep(1)
+            except AttributeError:
+                break
+
+        print(f"Total articles for {search}: {len(articles)}")
+        company_articles[search] = articles
+    return company_articles
+
+
+def get_int_label(label: str):
+    if label == "negative":
+        return 0
+    if label == "positive":
+        return 1
+    if label == "neutral":
+        return 2
+
+
+def get_labels(companies, num_days_back, max_articles_per_search):
+    """Get the labels for the headlines"""
+
+    pipe = pipeline(
+        "text-classification",
+        model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis",
+    )
+
+    from_date = datetime.now() - timedelta(days=num_days_back)
+
+    companies = get_news_headlines(
+        search_companies=companies,
+        from_date=from_date,
+        max_articles_per_search=max_articles_per_search,
+    )
+
+    for _, articles in companies.items():
+        for article in articles:
+            if article["text"][-1] != "?":
+                space = ". "
+            else:
+                space = " "
+            article["text"] = article["headline"] + space + article["text"]
+            article["label"] = get_int_label(pipe(article["text"])[0]["label"])
+            del article["headline"]
+            del article["posted"]
+
+    return companies
+
+
+def get_embedded_features(
+    companies: list = ["tesla"],
+    num_days_back: int = 1,
+    max_articles_per_search: int = 50,
+):
+    tokenizer = GPT2TokenizerFast.from_pretrained("Xenova/text-embedding-ada-002")
+
+    scraped_data = get_labels(
+        companies=companies,
+        num_days_back=num_days_back,
+        max_articles_per_search=max_articles_per_search,
+    )
+    get_embedded_features = []
+    for _, articles in scraped_data.items():
+        for article in articles:
+            article["text"] = tokenizer.encode(article["text"])
+            get_embedded_features.append(article)
+
+    return get_embedded_features
